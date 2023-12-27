cfcbac4fdda10b2fb0f3e0a7b35df6c1f3b581ea
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-12-27 14:30:08 -0300

    add

--- back/CRUDMascote/CRUDMascote/Controllers/MascoteController.cs
+++ back/CRUDMascote/CRUDMascote/Controllers/MascoteController.cs
@@ -28,11 +28,63 @@ namespace CRUDMascote.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
-            }           
+            }
 
-        }  
-        
+        }
 
-    }  
-        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var mascote = await _contexto.Mascotes.FirstOrDefaultAsync(m => m.Id == id);
+                if (mascote == null)
+                    return NotFound();
+                return Ok(mascote);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var delmascote = await _contexto.Mascotes.FirstOrDefaultAsync(m => m.Id == id);
+
+                if (delmascote == null)
+                    return NotFound();
+
+                _contexto.Mascotes.Remove(delmascote);
+                await _contexto.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(Mascote mascote)
+        {
+            try
+            {
+                mascote.DataCriacao = DateTime.Now;
+                _contexto.Mascotes.AddAsync(mascote);
+                await _contexto.SaveChangesAsync();
+               
+                return CreatedAtAction("Get", new {id = mascote.Id}, mascote);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
 }
